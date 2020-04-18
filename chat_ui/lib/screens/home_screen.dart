c3b4d9fd2a4dda3e794ffa44ab28650b94c61eba
Guilherme Esdras <guilherme.esdras@outlook.com>
2020-04-18 19:02:43 -0300

    Update

--- chat_ui/lib/screens/home_screen.dart
+++ chat_ui/lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:chat_ui/widgets/category_selector.dart';
+import 'package:chat_ui/widgets/favorite.contacts.dart';
+import 'package:chat_ui/widgets/recent_chats.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,7 +14,49 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        centerTitle:
+            true, // Por padrão, no iOS os títulos em um appBar são centralizados, mas no Android não, esta opção centraliza no android também.
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 28.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Chats',
+          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 28.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          CategorySelector(),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  )),
+              child: Column(children: <Widget>[
+                FavoriteContacts(),
+                RecentChats(),
+              ]),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
